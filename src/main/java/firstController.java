6b189b9cf43400f956cd0ff2ee37236d6fff219d
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-11-27 23:31:22 -0600

    controller added

--- src/main/java/firstController.java
+++ src/main/java/firstController.java
@@ -1,3 +1,10 @@
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class firstController {
+
+    public String first_api(){
+        return "Welcome to the home page";
+    }
     
 }
